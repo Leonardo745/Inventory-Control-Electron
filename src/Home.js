bf4ff07b385fb4190a7a44b4f593e23b23baf722
Leonardo Mariotto <leonardo_mariotto@yahoo.com.br>
2022-05-31 22:53:45 -0300

    Improving AddItem Func and adding AddCategory

--- src/Home.js
+++ src/Home.js
@@ -12,21 +12,56 @@ export default function Home() {
     var prod = produtos;
     args.id = produtos.count + 1;
 
-    var map = produtos.category.map((item) => {
-      console.log(item);
+    var catExist = false;
+    produtos.category.forEach((item) => {
+      //console.log(item);
       if (item.nameCat == cat) {
-        item.itens.push(args);
+        catExist = true;
+        console.log("Categoria '" + cat + "' encontrada");
       }
-      return item;
     });
-    console.log(map);
-    prod.category = map;
-    prod.count++;
-    setProdutos(prod);
 
-    let result = await Api.saveData(prod);
-    console.log(result);
+    if (catExist) {
+      var map = produtos.category.map((item) => {
+        //console.log(item);
+        if (item.nameCat == cat) {
+          item.itens.push(args);
+          console.log("Produto '" + args.name + "' Adicionado");
+        }
+        return item;
+      });
+      //console.log(map);
+      prod.category = map;
+      prod.count++;
+
+      setProdutos(prod);
+      let result = await Api.saveData(prod);
+      console.log("Retorno da Api: " + result);
+    } else {
+      console.log("Categoria '" + cat + "' não encontrada");
+    }
   }
+
+  async function AddCategory(cat) {
+    var catExist = false;
+    produtos.category.forEach((item) => {
+      //console.log(item);
+      if (item.nameCat == cat) {
+        catExist = true;
+        console.log("Categoria '" + cat + "' já existe!");
+      }
+    });
+    if (!catExist) {
+      var prod = produtos;
+      var newCat = { nameCat: cat, itens: [] };
+      prod.category.push(newCat);
+      console.log("Categoria '" + cat + "' Adicionada");
+      setProdutos(prod);
+      let result = await Api.saveData(prod);
+      console.log("Retorno da Api: " + result);
+    }
+  }
+
   async function handleLoadData() {
     const result = await Api.readData();
     setProdutos(result === undefined ? null : result);
@@ -47,7 +82,7 @@ export default function Home() {
         <div className="headerBtnContainer">
           <button
             onClick={() =>
-              AddItem("Sala", {
+              AddItem("Quarto", {
                 id: 0,
                 name: "Random",
                 quant: 100,
@@ -59,7 +94,7 @@ export default function Home() {
           </button>
           <button
             onClick={() => {
-              console.log(produtos);
+              AddCategory("Banheiro");
             }}
           >
             Adicionar Categoria
