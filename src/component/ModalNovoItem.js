91a7ce4f8f5c1261c36937debe83a5185e845d07
Leonardo Mariotto <leonardo_mariotto@yahoo.com.br>
2022-06-04 02:34:48 -0300

    Modal NovoItem functional

--- src/component/ModalNovoItem.js
+++ src/component/ModalNovoItem.js
@@ -1,42 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/modal.css';
 import '../../styles/styles.css';
-import logo from '../../public/images/logo.svg';
-import * as myModule from '../Home';
 
 const ModalNovoItem = props => {
-  const [Category, SetCategory] = useState();
-  const [Args, SetArgs] = useState();
+  const [produtos, setProdutos] = useState(null);
 
-  const [nome, SetNome] = useState();
-  const [desc, SetDesc] = useState();
-  const [preco, SetPreco] = useState();
-  const [quant, SetQuant] = useState();
+  const [nome, setNome] = useState('');
+  const [cat, seCat] = useState('');
+  const [desc, setDesc] = useState('');
+  const [preco, setPreco] = useState(0);
+  const [quant, setQuant] = useState(0);
+  const [errorVisibility, setErrorVisibility] = useState(false);
+
+  async function AddItem(cat, args) {
+    var prod = produtos;
+    args.id = produtos.count + 1;
+
+    var catExist = false;
+    produtos.category.forEach(item => {
+      if (item.nameCat == cat) {
+        catExist = true;
+        console.log("Categoria '" + cat + "' encontrada");
+      }
+    });
+
+    if (catExist) {
+      var map = produtos.category.map(item => {
+        if (item.nameCat == cat) {
+          item.itens.push(args);
+          console.log("Produto '" + args.name + "' Adicionado");
+        }
+        return item;
+      });
+      prod.category = map;
+      prod.count++;
+
+      setProdutos(Object.create(prod));
+      let result = await Api.saveData(prod);
+      console.log('Retorno da Api: ' + result);
+      return 0;
+    } else {
+      console.log("Categoria '" + cat + "' não encontrada");
+      return 1;
+    }
+  }
+
+  async function handleClickConfirm() {
+    var response = await AddItem(cat, {
+      id: 0,
+      name: nome,
+      quant: quant,
+      value: preco,
+      description: desc,
+      selectedQuant: 0,
+    });
+
+    if (response === 0) {
+      closeModal();
+    } else {
+      setErrorVisibility(true);
+    }
+  }
+
+  function closeModal() {
+    props.onClose();
+
+    setNome('');
+    seCat('');
+    setDesc('');
+    setPreco(0);
+    setQuant(0);
+    setErrorVisibility(false);
+  }
+
+  useEffect(() => {
+    setProdutos(props.produtos);
+  }, [props.produtos]);
 
   if (!props.show) {
     return null;
   }
   return (
-    <div className="modal" onClick={props.onClose}>
+    <div className="modal" onClick={closeModal}>
+      {errorVisibility ? (
+        <div className="error">
+          <span className="error-text">Categoria {cat} não encontrada</span>
+        </div>
+      ) : null}
+
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className="modal-header">
           <h4 className="modal-title">Novo Item</h4>
         </div>
         <div className="modal-body-Novoitem">
           <p>Nome:</p>
-          <input className="modal-searchbar-NovoItem" type="text" id="fname" name="fname" onChange={e => SetNome(e.target.value)} />
+          <input maxLength={30} className="modal-searchbar-NovoItem" type="text" onChange={e => setNome(e.target.value)} />
           <p>Categoria:</p>
-          <input className="modal-searchbar-NovoItem" type="text" id="fname1" name="fname" onChange={e => SetCategory(e.target.value)} />
+          <input
+            maxLength={18}
+            className="modal-searchbar-NovoItem"
+            type="text"
+            onChange={e => {
+              seCat(e.target.value);
+              setErrorVisibility(false);
+            }}
+          />
           <p>Descricao:</p>
-          <input className="modal-searchbar-NovoItem" type="text" id="fname2" name="fname" onChange={e => SetDesc(e.target.value)} />
+          <input className="modal-searchbar-NovoItem" type="text" onChange={e => setDesc(e.target.value)} />
           <p>Preco:</p>
-          <input className="modal-searchbar-NovoItem" type="text" id="fname3" name="fname" onChange={e => SetPreco(e.target.value)} />
+          <input className="modal-searchbar-NovoItem" type="number" onChange={e => setPreco(Number(e.target.value))} />
           <p>Quantidade em estoque inicial:</p>
-          <input className="modal-searchbar-NovoItem" type="text" id="fname4" name="fname" onChange={e => SetQuant(e.target.value)} />
+          <input className="modal-searchbar-NovoItem" type="number" onChange={e => setQuant(Number(e.target.value))} />
         </div>
         <div className="modal-footer">
-          <button onClick={(() => SetArgs([nome, desc, preco, quant]), console.log(Args))}>Alterar</button>
-          <button onClick={() => myModule.AddItem[(Category, Args)]}>Confirmar</button>
+          <button onClick={() => handleClickConfirm()}>Confirmar</button>
         </div>
       </div>
     </div>
